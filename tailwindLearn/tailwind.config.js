d9ac3995f7926979396c44c75f623816dde0dff4
Snehal Tawade <snehaltawade99@gmail.com>
2024-11-13 08:18:15 +0530

    tailwind first commit

---
+++ tailwindLearn/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// npx tailwindcss -i ./style.css -o ./output.css --watch
